c678291b9299b8b35c65fb74956a11b5c4bae987
ny <ny@debian.deb>
2019-11-21 00:54:59 -0500

    comment lsp java in init

--- init.el
+++ init.el
@@ -1,9 +1,9 @@
 (require 'package)
-;;(setq package-archives '(("gnu"   . "http://mirrors.tuna.tsinghua.edu.cn/elpa/gnu/")
-;;                         ("melpa" . "http://mirrors.tuna.tsinghua.edu.cn/elpa/melpa/")))
+(setq package-archives '(("gnu"   . "http://mirrors.tuna.tsinghua.edu.cn/elpa/gnu/")
+                      ("melpa" . "http://mirrors.tuna.tsinghua.edu.cn/elpa/melpa/")))
 
-(add-to-list 'package-archives
-         '("melpa" . "http://melpa.org/packages/") t)
+;;(add-to-list 'package-archives
+;;         '("melpa" . "http://melpa.org/packages/") t)
 
 (package-initialize)
 
@@ -19,7 +19,8 @@
 (add-to-list 'load-path "~/.emacs.d/custom")
 
 (require 'setup-general)
-(require 'setup-lsp-java)
+;; Do not develop java in emacs, so suck!
+;;(require 'setup-lsp-java)
 (require 'setup-treemacs)
 (require 'setup-helm)
 (require 'setup-helm-gtags)
@@ -46,4 +47,7 @@
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
    (quote
-    ("669e02142a56f63861288cc585bee81643ded48a19e36bfdf02b66d745bcc626" default))))
+    ("b96849affc3dbee03dbed408ee72c6d394dbd5f28dcd222935ea4656869df4ec" "f1c21ec508b17eb15e8a30bc77951a50a88929e74a377edc9b6d5d51397d88c8" "ce6129f9462fd637890eff6afc14a4f5e35a41acf2569cedcb1bae5d3ec52f77" "669e02142a56f63861288cc585bee81643ded48a19e36bfdf02b66d745bcc626" default)))
+ '(package-selected-packages
+   (quote
+    (helm-gtags helm yasnippet use-package treemacs-projectile treemacs-magit treemacs-icons-dired treemacs-evil solarized-theme rainbow-delimiters lsp-ui lsp-java ggtags dap-mode company-lsp bing-dict auto-highlight-symbol atom-one-dark-theme))))
